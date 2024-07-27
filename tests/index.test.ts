34a4c94c733c29df85d9464157c58051106bb860
Sherif Hoti <sherifhoti1235@hotmail.com>
2024-07-28 00:24:57 +0200

    Some changes

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,8 @@
 import { assertEquals } from "https://deno.land/std@0.224.0/testing/asserts.ts";
-import {
-  isWindowBrowserOptional,
-  isDocumentBrowserOptional,
-} from "../utils/isBrowserOptional.ts";
+import { isWindowBrowserOptional } from "../utils/isBrowserOptional.ts";
 import * as z from "npm:zod@3.23.8";
 import { option, either } from "npm:fp-ts";
-import Storagef from "../localStorage/Storagef.ts";
+import { getItem, setItem } from "../localStorage/storagef.ts";
 
 Deno.test("isWindowBrowserOptional", () => {
   assertEquals(isWindowBrowserOptional(), option.some(window));
@@ -19,78 +16,95 @@ Deno.test("localStorage", () => {
   assertEquals(localStorage.getItem(""), null);
 });
 
-// Deno.test("getItemLocalStorageSucess", () => {
-//   const localStorage = window.localStorage;
-//   const key = "testing";
-//   const value = { test: "hello" };
-//   localStorage.setItem(key, JSON.stringify(value));
-//   const schema = z.object({
-//     test: z.string(),
-//   });
-//   // const schema = z.string();
-//   assertEquals(getItem(key, schema), either.right(value));
-// });
-
-// Deno.test("getItemLocalStorageFail", () => {
-//   const localStorage = window.localStorage;
-//   const key = "testing";
-//   const value = '{ test: "hello" }';
-//   localStorage.setItem(key, JSON.stringify(value));
-//   const schema = z.object({
-//     test: z.string(),
-//   });
-//   // const schema = z.string();
-//   const result = getItem(key, schema);
-//   if (result._tag === "Left") {
-//     assertEquals(result.left.type, "SCHEMA_NOT_VALID");
-//   }
-// });
-
-// Deno.test("getItemLocalStorageStringSuccess", () => {
-//   const localStorage = window.localStorage;
-//   const key = "testing";
-//   const value = '{ test: "hello" }';
-//   localStorage.setItem(key, JSON.stringify(value));
-//   // const schema = z.object({
-//   //   test: z.string(),
-//   // });
-//   const schema = z.string();
-//   assertEquals(getItem(key, schema), either.right(value));
-// });
-
-Deno.test("Storagef class get Item not found", () => {
+Deno.test("getItemLocalStorageSucess", () => {
   const localStorage = window.localStorage;
-  const storage = new Storagef(localStorage);
-  assertEquals(
-    storage.getItem("key1", z.string()),
-    either.left({
-      type: "STORAGE_ITEM_NOT_FOUND" as const,
-      error: new Error("STORAGE_ITEM_NOT_FOUND"),
-    })
-  );
+  const key = "testing";
+  const value = { test: "hello" };
+  localStorage.setItem(key, JSON.stringify(value));
+  const schema = z.object({
+    test: z.string(),
+  });
+  // const schema = z.string();
+  assertEquals(getItem(key, schema), either.right(value));
 });
 
-Deno.test("Storagef class get Item found and its an object", () => {
+Deno.test("getItemLocalStorageFail", () => {
   const localStorage = window.localStorage;
-  localStorage.setItem("key", '{ "test":"value" }');
-  const storage = new Storagef(localStorage);
-  assertEquals(
-    storage.getItem(
-      "key",
-      z.object({
-        test: z.string(),
-      })
-    ),
-    either.right({ test: "value" })
-  );
+  const key = "testing";
+  const value = '{ test: "hello" }';
+  localStorage.setItem(key, JSON.stringify(value));
+  const schema = z.object({
+    test: z.string(),
+  });
+  // const schema = z.string();
+  const result = getItem(key, schema);
+  if (result._tag === "Left") {
+    assertEquals(result.left.type, "SCHEMA_NOT_VALID");
+  }
 });
 
-Deno.test("Storagef class set Item", () => {
+Deno.test("getItemLocalStorageStringSuccess", () => {
   const localStorage = window.localStorage;
-  const storage = new Storagef(localStorage);
-
-  assertEquals(
-    storage.setItem("key", '{ "test":"value" }', z.string()),
-    either.right({ key: "key", value: '{ "test":"value" }' })
-  );
+  const key = "testing";
+  const value = '{ test: "hello" }';
+  localStorage.setItem(key, JSON.stringify(value));
+  // const schema = z.object({
+  //   test: z.string(),
+  // });
+  const schema = z.string();
+  assertEquals(getItem(key, schema), either.right(value));
 });
+
+Deno.test("setItemLocalStorageSucess", () => {
+  const schema = z.object({
+    test: z.string(),
+  });
+
+  const key = "set_item_testing";
+  const value = {
+    test: "test_value",
+  };
+
+  setItem(key, value, schema);
+
+  const localStorage = window.localStorage;
+
+  assertEquals(localStorage.getItem(key), JSON.stringify(value));
+});
+
+// Deno.test("Storagef class get Item not found", () => {
+//   const localStorage = window.localStorage;
+//   const storage = new Storagef(localStorage);
+//   assertEquals(
+//     storage.getItem("key1", z.string()),
+//     either.left({
+//       type: "STORAGE_ITEM_NOT_FOUND" as const,
+//       error: new Error("STORAGE_ITEM_NOT_FOUND"),
+//     })
+//   );
+// });
+
+// Deno.test("Storagef class get Item found and its an object", () => {
+//   const localStorage = window.localStorage;
+//   localStorage.setItem("key", '{ "test":"value" }');
+//   const storage = new Storagef(localStorage);
+//   assertEquals(
+//     storage.getItem(
+//       "key",
+//       z.object({
+//         test: z.string(),
+//       })
+//     ),
+//     either.right({ test: "value" })
+//   );
+// });
+
+// Deno.test("Storagef class set Item", () => {
+//   const localStorage = window.localStorage;
+//   const storage = new Storagef(localStorage);
+
+//   assertEquals(
+//     storage.setItem("key", '{ "test":"value" }', z.string()),
+//     either.right({ key: "key", value: '{ "test":"value" }' })
+//   );
+// });
